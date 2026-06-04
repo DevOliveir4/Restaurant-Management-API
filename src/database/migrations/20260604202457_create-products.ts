8d5f642b51099c9722237d37c0d57d7408877f6c
Gabriel Oliveira <carmooliveiragabriel@gmail.com>
2026-06-04 17:35:39 -0300

    criando banco de dados com knex

--- src/database/migrations/20260604202457_create-products.ts
+++ src/database/migrations/20260604202457_create-products.ts
@@ -1,11 +1,20 @@
+import { table } from "console";
 import type { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
-    
+    await knex.schema.createTable("products", (table) => {
+        table.increments("product_id").primary()
+        table.text("product_name").notNullable()
+        table.decimal("product_price").notNullable()
+        table.timestamp("product_created_at").defaultTo(knex.fn.now())
+        table.timestamp("product_updated_at").defaultTo(knex.fn.now())
+
+    })
 }
 
 
 export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable("products")
 }
 
