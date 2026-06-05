36834481accbacd7f133c685376d5677efcea503
Gabriel Oliveira <carmooliveiragabriel@gmail.com>
2026-06-05 10:50:35 -0300

    criando e populando tabela de mesas

---
+++ src/database/migrations/20260605133919_crate-tables.ts
@@ -0,0 +1,19 @@
+import { table } from "console";
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable("tables", (table) => {
+        table.increments("table_id").primary(),
+        table.integer("table_number").notNullable(),
+        table.timestamp("table_crated_at").defaultTo(knex.fn.now()),
+        table.timestamp("table_updated_at").defaultTo(knex.fn.now())
+
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable("tables")
+}
+
