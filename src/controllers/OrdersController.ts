ff921974c5cbede61c8dccbf95557a680ee93ec9
Gabriel Oliveira <carmooliveiragabriel@gmail.com>
2026-06-08 11:42:06 -0300

    criando rota para listar pedidos de uma mesa e rota para calcular total e feitos alguns ajustes na modelagem do banco de dados

--- src/controllers/OrdersController.ts
+++ src/controllers/OrdersController.ts
@@ -10,10 +10,10 @@ export class OrdersController {
             const bodySchema = z.object({
                 table_session_id: z.number(),
                 product_id: z.number(),
-                order_product_quantity: z.number()
+                product_quantity: z.number()
             })
 
-            const { table_session_id, product_id, order_product_quantity } = bodySchema.parse(request.body)
+            const { table_session_id, product_id, product_quantity } = bodySchema.parse(request.body)
 
             const session = await knex<TablesSessionsRepository>("tables_sessions").select().where({ table_session_id: table_session_id }).first()
             
@@ -34,13 +34,48 @@ export class OrdersController {
             await knex<OrdersRepository>("orders").insert({
                 table_session_id: table_session_id,
                 product_id: product_id,
-                order_product_quantity: order_product_quantity,
-                order_price: product.product_price * order_product_quantity
+                product_price: product.product_price,
+                product_quantity: product_quantity,
+                order_price: product.product_price * product_quantity
             })
 
-            
-
             return response.status(201).json(product)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async index(request: Request, response: Response, next: NextFunction) {
+        try {
+            const { table_session_id } = request.params
+
+            const orders = await knex("orders")
+            .select(
+                "orders.order_id", 
+                "orders.table_session_id",
+                "orders.product_id",
+                "products.product_name",
+                "orders.product_price", 
+                "orders.product_quantity", 
+                "orders.order_price",
+                "orders.order_created_at",
+                "orders.order_updated_at"
+            ).join("products", "products.product_id", "orders.product_id").where({ table_session_id: table_session_id }).orderBy("orders.order_created_at", "desc")
+
+            return response.json(orders)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async show(request: Request, response: Response, next: NextFunction) {
+        try {
+            const { table_session_id } = request.params
+
+            const order = await knex("orders").select(knex.raw("COALESCE(SUM(order_price), 0) AS total"), knex.raw("COALESCE(SUM(product_quantity), 0) AS products_quantity")).where({ table_session_id: table_session_id}).first()
+
+
+            return response.json(order)
         } catch (error) {
             next(error)
         }
--- src/routes/ordersRoutes.ts
+++ src/routes/ordersRoutes.ts
@@ -5,4 +5,6 @@ export const  ordersRoutes = Router()
 const ordersController = new OrdersController()
 
 ordersRoutes.put("/", ordersController.create)
+ordersRoutes.get("/table-session/:table_session_id", ordersController.index)
+ordersRoutes.get("/table-session/:table_session_id/total", ordersController.show)
 
